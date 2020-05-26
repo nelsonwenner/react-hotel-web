58d5898b96b956edf46f9ca5c48420377fd4dbe5
wenner <nelsondiasdemedeiros@gmail.com>
2020-05-26 00:13:53 -0300

    chore: add gallery from images of hotels

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 import Header from '../../components/Header/Header';
 import Banner from '../../components/Banner/Banner';
+import InputGroup from '../../components/common/InputGroup/InputGroup';
+import Hotels from '../../components/Hotels/Hotels';
 
 const Home = () => {
   return (
     <>
+    
       <Header />
       <Banner />
+      <InputGroup />
+      <Hotels />
+
     </>
   )
 }
